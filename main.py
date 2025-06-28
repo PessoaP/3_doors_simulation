2558302262f354ac33b2318f340ffd958756747b
Pedro Pessoa <67424184+PessoaP@users.noreply.github.com>
2025-06-28 13:53:54 -0700

    reset_now

--- main.py
+++ main.py
@@ -102,8 +102,10 @@ elif st.session_state.phase == "switch":
             st.error("😢 You got a goat.")
 
         if st.button("🔁 Play Again"):
+            st.session_state.started = False
             st.session_state.phase = "pick"
             st.session_state.doors = None
             st.session_state.choice = None
             st.session_state.monty_opens = None
             st.rerun()
+

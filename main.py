7eb1b002d6d88930b6f67b83ff18885f06961e18
Pedro Pessoa <67424184+PessoaP@users.noreply.github.com>
2025-06-28 14:28:49 -0700

    restart14

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Monty Hall Simulator", page_icon="🚪")
+st.cache_data.clear()
 
 # -------------------------------
 # Introductory Explanation
